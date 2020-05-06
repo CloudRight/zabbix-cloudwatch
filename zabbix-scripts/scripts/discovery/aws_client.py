b4db0da6833817c6ad993c15bfc6a84861460b2a
Juho Vanhanen <juho.vanhanen@pandia.fi>
2020-05-06 16:28:44 +0300

    Load specified profile from aws config

--- zabbix-scripts/scripts/discovery/aws_client.py
+++ zabbix-scripts/scripts/discovery/aws_client.py
@@ -4,9 +4,10 @@ import boto3
 
 class AWSClient(object):
     "Basic object for AWS services discovery"
-    def __init__(self, account, service, region):
+    def __init__(self, profile, service, region):
         "Initializes Boto3 client for specified service"
-        self.client = boto3.client(
+        session = boto3.Session(profile_name=profile)
+        self.client = session.client(
             service,
             region_name=region)
         self.region = region
