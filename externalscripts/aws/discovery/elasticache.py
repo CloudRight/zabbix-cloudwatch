0a2ab587eabe9bb56587e8a34787179e3a27bfe5
Frank Klaassen <frank@cloudright.nl>
2020-12-17 14:27:29 +0100

    add other cache instance types

--- externalscripts/aws/discovery/elasticache.py
+++ externalscripts/aws/discovery/elasticache.py
@@ -8,14 +8,19 @@ class Discoverer(BasicDiscoverer):
         data = []
 
         for instance in response["CacheClusters"]:
+            memory_bytes = 0 # Fall back if other instance type is provided
             if instance["CacheNodeType"] == "cache.t2.micro":
-                memory_bytes = int(555 * pow(1024, 2))
+                memory_bytes = int(0.555 * pow(1024, 3))
             if instance["CacheNodeType"] == "cache.t2.small":
                 memory_bytes = int(1.55 * pow(1024, 3))
             if instance["CacheNodeType"] == "cache.t2.medium":
                 memory_bytes = int(3.22 * pow(1024, 3))
-            if instance["CacheNodeType"] == "cache.m3.medium":
-                memory_bytes = int(2.78 * pow(1024, 3))
+            if instance["CacheNodeType"] == "cache.t3.micro":
+                memory_bytes = int(0.5 * pow(1024, 3))
+            if instance["CacheNodeType"] == "cache.t3.small":
+                memory_bytes = int(1.37 * pow(1024, 3))
+            if instance["CacheNodeType"] == "cache.t3.medium":
+                memory_bytes = int(3.09 * pow(1024, 3))
             try:
                 cache_replicationgroupid = "%s" % instance["ReplicationGroupId"]
             except:
